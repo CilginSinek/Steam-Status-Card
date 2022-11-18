e7d0fe95dc99eb3788df971202fba5dbb54d98b3
CilginSinek <cilginsinek58@gmail.com>
2022-11-18 11:16:36 +0300

    change steamcard

--- Steamcard.mjs
+++ Steamcard.mjs
@@ -6,30 +6,9 @@ const sumaryUrl = `https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0
 const badgeUrl =`https://api.steampowered.com/IPlayerService/GetSteamLevel/v1/?key=${webkey}&steamid=${Userid}`
 
 
-// var mypromise = new Promise((resolve, reject) => {
-//     fetch(userUrl)
-//         .then((response) => response.json())
-//         .then(function (items) {
-//             items.foreach((item) => {
-//                 console.log(item)
-//             })
-//         }).catch((err) => {console.log(err)})
-// })
-import express, { response } from 'express';
-const app = express();
+
 import fetch from 'node-fetch';
 import cors from 'cors';
 
-app.use(cors())
-// get summary
-app.get("/mysteamsumaryDONTGETTHAT", async (req, res) => {
-    const response = await fetch(sumaryUrl);
-    res.json(await response.json())
-})
-//get badge
-app.get("/mysteamlevelapiDONTGETTHAT", async (req, res) =>{
-    const response = await fetch(badgeUrl);
-    res.json(await response.json())
-})
-
-app.listen(3000)
+let Summaryfetch = await fetch(sumaryUrl, cors());
+console.log(Summaryfetch)
