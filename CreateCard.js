2f95560e9efda215b23a4cf3b01ab1605ce670c9
CilginSinek <cilginsinek58@gmail.com>
2022-11-19 12:46:25 +0300

    Beta | 0.1

--- CreateCard.js
+++ CreateCard.js
@@ -1,17 +1,23 @@
-let summary = await fetch("http://localhost:3000/mysteamsumaryDONTGETTHAT")
-.then(res => res.json())
-.then(data => {
-    return data
-});
-let level = await fetch("http://localhost:3000/mysteamlevelapiDONTGETTHAT")
-.then(res => res.json())
-.then(data => {
-    return data
+let Userid = "";
+// Ur own steam id64
+let webkey = ""
+// ur own steam web api key
+const sumaryUrl = `https://corsproxy.io/?https%3A%2F%2Fapi.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${webkey}&steamids=${Userid}`
+const badgeUrl =`https://corsproxy.io/?https%3A%2F%2Fapi.steampowered.com/IPlayerService/GetSteamLevel/v1/?key=${webkey}&steamid=${Userid}`
 
+const gamesURL = `https://corsproxy.io/?https%3A%2F%2Fapi.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=${webkey}&format=json&steamid=${Userid}&include_appinfo=1`
+
+
+let summary = await fetch(sumaryUrl)
+.then(res => res.json())
+.then(data => {
+    return data
 });
-console.log(summary)
+let level = await fetch(badgeUrl).then(res => res.json()).then(data => {return data});
+
+let mygame = await fetch(gamesURL).then(res => res.json()).then(data => {return data});
 console.log(level)
-
+console.log(summary)
 
 
 
@@ -36,13 +42,14 @@ function createAcard(userdata, leveldata) {
             leveltext = document.createElement('div'),
             levelcircle = document.createElement('div'),
                 levelcirclein = document.createElement('div'),
+                    levelcircletext = document.createElement('div'),
         badgearea = document.createElement('div'),
         isActive = document.createElement('div');
 
 
     firstdiv.classList.add("first")
     
-    const ilkdiv = document.querySelector("#ilkdiv");
+    const ilkdiv = document.querySelector(".Steamcard");
     
     ilkdiv.appendChild(firstdiv);
 
@@ -65,6 +72,7 @@ function createAcard(userdata, leveldata) {
         levelarea.appendChild(leveltext);
         levelarea.appendChild(levelcircle);
             levelcircle.appendChild(levelcirclein);
+                levelcirclein.appendChild(levelcircletext);
     firstdivrightarea.appendChild(badgearea);
     firstdivrightarea.appendChild(isActive);
     //firstdiv left area
@@ -75,7 +83,7 @@ function createAcard(userdata, leveldata) {
             avatararea.classList.add('OfflineColor');
             break;
         case 1:
-            if (userdata.response.players.gameextrainfo) {
+            if (userdata.response.players[0].gameextrainfo) {
                 avatararea.classList.add('lookingToPlayColor');
             } else {
                 avatararea.classList.add('OnlineColor');
@@ -104,6 +112,7 @@ function createAcard(userdata, leveldata) {
     }
     newImg.src = userdata.response.players[0].avatarfull;
 
+    avatarimg.classList.add('avatarCSS')
     //firstdiv Mid area
 
     usernick.textContent = `${userdata.response.players[0].personaname}`
@@ -114,58 +123,63 @@ function createAcard(userdata, leveldata) {
 
 
     // level
-    leveltext.textContent = "Level"
-    levelcirclein.textContent = `${leveldata.response.player_level}`
-    levelcircle.classList.add("circleCSS")
-    levelcirclein.classList.add("circle")
-    leveltext.classList.add("leveltext")
-    levelarea.classList.add("levelarea")
+    leveltext.textContent = "Level";
+    levelcircletext.textContent = `${leveldata.response.player_level}`;
+    levelcircle.classList.add("circleCSS");
+    levelcirclein.classList.add("circle");
+    levelcircletext.classList.add("circletext");
+    leveltext.classList.add("leveltext");
+    levelarea.classList.add("levelarea");
     //first digit of level
-    const firstdigitlevel = String(leveldata.response.player_level);
-    switch (firstdigitlevel){
+    const firstdigitlevel = String(leveldata.response.player_level)[0];
+    switch (parseInt(firstdigitlevel)){
         case 0:
-            levelcircle.classList.add("greycolor");
+            levelcircle.classList.add('color0');
             break;
         case 1:
-            levelcircle.classList.add("redcolor");
+            levelcircle.classList.add('color1');
             break;
         case 2:
-            levelcircle.classList.add("orangecolor");
+            levelcircle.classList.add('color2');
             break;
         case 3:
-            levelcircle.classList.add("yellowcolor");
+            levelcircle.classList.add('color3');
             break;
         case 4:
-            levelcircle.classList.add("greencolor");
+            levelcircle.classList.add('color4');
             break;
         case 5:
-            levelcircle.classList.add("bluecolor");
+            levelcircle.classList.add('color5');
             break;
         case 6:
-            levelcircle.classList.add("purplecolor");
+            levelcircle.classList.add('color6');
             break;
         case 7:
-            levelcircle.classList.add("pinkcolor");
+            levelcircle.classList.add('color7');
             break;
         case 8:
-            levelcircle.classList.add("browncolor");
+            levelcircle.classList.add('color8');
             break;
         case 9:
-            levelcircle.classList.add("lightbrowncolor");
+            levelcircle.classList.add('color9');
     };
 
     //status text
 
-    if(userdata.response.players.personastate ==!0){
+    if(userdata.response.players[0].personastate ==!0){
         if(userdata.response.players[0].gameextrainfo){
-            isActive.textContent = `Currently In-Game <br /> ${userdata.response.players[0].gameextrainfo}`
-            isActive.classList.add("yellowcolor")
+            isActive.textContent = `Currently In-Game ${userdata.response.players[0].gameextrainfo}`
+            isActive.classList.add("game-text-color")
+
         }
         else{
             isActive.textContent = `Currently Online`
+            isActive.classList.add("blue-text-color")
         }
-    }
-    else{
+    }else if(userdata.response.players[0].personastate == 3){
+        isActive.textContent = "Currently Idle"
+        isActive.classList.add("Idle-Text-Color")
+    }else{
         isActive.textContent = `Currently Offline`
     }
 
