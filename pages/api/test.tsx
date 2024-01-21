17eeb59efe902a357a6443f232a5f377d10678fb
CilginSinek <CilginSinek58@gmail.com>
2024-01-21 15:24:52 +0300

    update api/test

--- pages/api/test.tsx
+++ pages/api/test.tsx
@@ -8,7 +8,10 @@ export default async function handler(req: NextRequest) {
     const { searchParams } = req.nextUrl;
     const id = searchParams.get("id");
 
-    const steamHtml = await fetch(`https://steamcommunity.com/id/${id}`).catch(er=>{return er;});
+    const steamHtml = await fetch(`https://steamcommunity.com/id/${id}`).catch(er=>{
+        console.error(er)
+        return er
+    });
 
     return new ImageResponse(
         <div style={{display:"flex"}} >
