71153400727b8235a832c1179196ece5ed6f3c9b
CilginSinek <CilginSinek58@gmail.com>
2024-01-21 17:10:41 +0300

    test htmlAPi

--- pages/api/htmlAPi.tsx
+++ pages/api/htmlAPi.tsx
@@ -1,12 +1,31 @@
+import steamUser from '@/utils/steamUser';
+import axios from 'axios';
 import { NextApiRequest, NextApiResponse } from 'next';
- 
-export default function handler(
+
+export const config = {
+  runtime: 'nodejs',
+};
+
+export default async function handler(
     request: NextApiRequest,
     response: NextApiResponse,
   ) {
+    const { id } = request.query;
+    if(!id) {
+      response.status(401).json({ error: 'No id provided'});;
+    }
+
+    const steamHtml = await axios.get("https://steamcommunity.com/id/"+id)
+
+    if(steamHtml.status){
+      response.status(401).json({ error: 'Steam Profile not found'});
+    }
+
+    const steam = new steamUser(steamHtml.data);
+
+    const basicProfile = steam.getBasicProfile();
+
     response.status(200).json({
-      body: request.body,
-      query: request.query,
-      id: request.query.id
+      basicUser: basicProfile
     });
 }
