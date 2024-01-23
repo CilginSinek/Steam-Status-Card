704e5031bd032a90a625731c4c8e98032039c4e6
CilginSinek <CilginSinek58@gmail.com>
2024-01-23 16:50:57 +0300

    update test api

--- pages/api/testapi.tsx
+++ pages/api/testapi.tsx
@@ -6,13 +6,20 @@ import style from "@/styles/style";
 export const runtime = "edge";
 
 export default async function handler(req: NextRequest) {
-  try{
+  try {
     return new ImageResponse(
       (
         <div className="card container" style={style.card}>
-            <div style={{...style.imgSec, ...style.imgOnline}}>
-                <img src="https://avatars.akamai.steamstatic.com/a691782e9df50a0a5052ba0edb8df5b1789906a0_full.jpg" alt="avatar" />
+          <div style={style.user}>
+            <div style={style.info}>
+              <div style={{ ...style.imgSec, ...style.imgOnline }}>
+                <img
+                  src="https://avatars.akamai.steamstatic.com/a691782e9df50a0a5052ba0edb8df5b1789906a0_full.jpg"
+                  alt="avatar"
+                />
+              </div>
             </div>
+          </div>
         </div>
       ),
       {
@@ -20,8 +27,7 @@ export default async function handler(req: NextRequest) {
         height: 125,
       }
     );
-  }catch(e){
-    Response.json({error:e},{status:500})
+  } catch (e) {
+    Response.json({ error: e }, { status: 500 });
   }
-
 }
