a7dab8c4fe35eb9b430d4bf90c4ef166ffffede4
CilginSinek <CilginSinek58@gmail.com>
2024-01-23 17:03:24 +0300

    update test api

--- pages/api/testapi.tsx
+++ pages/api/testapi.tsx
@@ -2,20 +2,81 @@
 import { ImageResponse } from "@vercel/og";
 import { NextRequest } from "next/server";
 import style from "@/styles/style";
+import { basicUserInfo } from "@/utils/types";
 
 export const runtime = "edge";
 
 export default async function handler(req: NextRequest) {
   try {
+    const { searchParams } = req.nextUrl;
+    const id = searchParams.get("id");
+    const costumeComponent = searchParams.get("costume");
+
+    if (!id) {
+      return new ImageResponse(<>{'Visit with "?id=vercel"'}</>, {
+        width: 1200,
+        height: 630,
+      });
+    }
+
+    const steamData = await fetch(process.env.URL + "/api/htmlAPi?id=" + id, {
+      method: "GET",
+    });
+
+    if (steamData.status !== 200) {
+      return Response.json({
+        error: "Steam Profile not found",
+      });
+    }
+
+    const AllData: any = await steamData.json();
+
+    const basicUser: basicUserInfo = AllData.basicUser;
+
+    const mainLevelDef = () => {
+      if (basicUser.userInfo.level >= 100) {
+        return Math.floor(basicUser.userInfo.level / 100) * 100;
+      } else {
+        return Math.floor(basicUser.userInfo.level / 10) * 10;
+      }
+    };
+
+    const levelClass = style["friendPlayerLevel_lvl_" + mainLevelDef()];
+
+    const heightFunc = () => {
+      if (basicUser.status.statusType === "In-Game") {
+        return 250;
+      } else {
+        return 127;
+      }
+    };
+
+    const userNameFunc = (mystr: string) => {
+      if (mystr.length > 16) {
+        return mystr.substring(0, 13) + "...";
+      } else {
+        return mystr;
+      }
+    };
+
     return new ImageResponse(
       (
         <div className="card container" style={style.card}>
           <div style={style.user}>
             <div style={style.info}>
-              <div style={{ ...style.imgSec, ...style.imgOnline }}>
+              <div
+                style={{
+                  ...style.imgSec,
+                  ...(basicUser.status.statusType === "Online"
+                    ? style.imgOnline
+                    : basicUser.status.statusType === "In-Game"
+                    ? style.imgInGame
+                    : style.imgOffline),
+                }}
+              >
                 <img
                   style={style.avatar}
-                  src="https://avatars.akamai.steamstatic.com/a691782e9df50a0a5052ba0edb8df5b1789906a0_full.jpg"
+                  src={basicUser.userInfo.avatar}
                   alt="avatar"
                 />
               </div>
@@ -25,7 +86,7 @@ export default async function handler(req: NextRequest) {
       ),
       {
         width: 360,
-        height: 125,
+        height: heightFunc(),
       }
     );
   } catch (e) {
