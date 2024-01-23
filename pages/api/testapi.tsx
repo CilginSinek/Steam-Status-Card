6ad4ea13cae817c1f157227439d22372722f9c6a
CilginSinek <CilginSinek58@gmail.com>
2024-01-23 03:54:09 +0300

    test single image

---
+++ pages/api/testapi.tsx
@@ -0,0 +1,27 @@
+/* eslint-disable @next/next/no-img-element */
+import { ImageResponse } from "@vercel/og";
+import { NextRequest } from "next/server";
+import style from "@/styles/style";
+
+export const runtime = "edge";
+
+export default async function handler(req: NextRequest) {
+  try{
+    return new ImageResponse(
+      (
+        <div className="card container" style={style.card}>
+            <div style={{...style.imgSec, ...style.imgOnline}}>
+                <img src="https://avatars.akamai.steamstatic.com/a691782e9df50a0a5052ba0edb8df5b1789906a0_full.jpg" alt="avatar" />
+            </div>
+        </div>
+      ),
+      {
+        width: 360,
+        height: 125,
+      }
+    );
+  }catch(e){
+    Response.json({error:e},{status:500})
+  }
+
+}
